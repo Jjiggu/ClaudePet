e434444cc1de5b49f2361a01d34e65b755a3480d
Jihun Kim <zh1229@naver.com>
2026-04-02 22:17:00 +0900

    Initial Commit

---
+++ ClaudePet/ClaudePetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudePetApp.swift
+//  ClaudePet
+//
+//  Created by 김지훈 on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudePetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudePet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClaudePet
+//
+//  Created by 김지훈 on 4/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
